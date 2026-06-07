ada179aee4a589dce5dd3451d0a361f424e71fbc
Joram <jorampoha33@gmail.com>
2026-06-07 15:34:20 +0200

    Implement navigation and alert box functions
    Added functions to open and hide navigation and alert boxes.

---
+++ main.js
@@ -0,0 +1,37 @@
+
+// OPEN NAVIGATION 
+function reveal() {
+  // Tab to edit
+  
+  document.getElementById("open").style.display = "block";
+}
+
+
+
+// HIDE NAVIGATION 
+function hide() {
+  // Tab to edit
+  
+  document.getElementById("open").style.display = "none";
+}
+
+// OPEN ALERT BOX FOR DOWNLOADING
+
+function alert() {
+  // Tab to edit
+  
+  document.getElementById("openAlert").style.display = "block";
+  
+  
+}
+
+// CLOSE ALERT BOX FOR DOWNLOADING
+
+
+function closeAlert() {
+  // Tab to edit
+  
+  document.getElementById("openAlert").style.display = "none";
+ 
+  
+}
